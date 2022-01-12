f2c3d49fbeacd216feeff0681428feb0f1964b0d
pydeveloper510 <85598258+pydeveloper510@users.noreply.github.com>
2022-01-13 03:13:41 +0800

    Update color_log.js

--- color_log.js
+++ color_log.js
@@ -1,3 +1,17 @@
+(function() { 
+  var old = console.log; 
+  console.log("> Log Date Format DD/MM/YY HH:MM:SS - UTCString");
+  console.log = function() { 
+    var n = new Date(); 
+    var d = ("0" + (n.getDate().toString())).slice(-2), 
+        m = ("0" + ((n.getMonth() + 1).toString())).slice(-2), 
+        y = ("0" + (n.getFullYear().toString())).slice(-2), 
+        t = n.toUTCString().slice(-13, -4); 
+    Array.prototype.unshift.call(arguments, "[" + d + "/" + m + "/" + y + t + "]"); 
+    old.apply(this, arguments); 
+  } 
+})();
+
 console.log('\u001b[32m\u001b[1m Login user: %d\u001b[0m ', 10);
 
 console.log("Log Info");
